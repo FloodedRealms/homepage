48c0bd8439386368bba9d8023f839ba7834a66d1
ShockTohp <shocktohp@shocktohp.xyz>
2023-11-17 14:05:33 -0500

    whimsy

---
+++ funlinks.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Flooded Cove - Fun</title>
+		<link rel="stylesheet" href="style.css">
+		<link rel="icon" type="image/x-icon" href="/assets/images/favicon.png">
+
+	</head>
+	<body>
+		<div id="container">
+			<div class="topbar"></div>
+			<div id="flex">
+				<main>
+					<div class="title">Fun on the web</div>
+					<div class="content">
+						<p>
+						There are still fun sites on the web. These are not related to anything
+						ttrpg, they are just currated oddities that people might be interested in.
+						</p>
+						<ul>
+							<li><a href="https://trypap.com/">Passsive Agressive
+							       Password</a></li>
+							       <li><a href="https://pointerpointer.com/">Pointer
+									       Pointer</a></li>
+							       <li><a href="https://eelslap.com/">Eel Slap</a></li>
+							       <li><a href="https://longdogechallenge.com/">Long
+									       Doge</a></li>
+							       <li><a href="https://theuselessweb.com/">Useless Website
+									       Finder</a></li>
+							       <li><a href="https://zoomquilt.org/">Zoom Quilt</a></li>
+							       <li><a href="https://hemansings.com/">A classic</a></li>
+					</div>
+				</main>
+				<aside>
+					<?php include 'nav.html'; ?>
+				</aside>
+			</div>
+		</div>
+
+	</body>
+</html>
