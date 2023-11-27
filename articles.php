f94c5bb44c9b2404ffb2fe305841b2673deb4095
ShockTohp <shocktohp@shocktohp.xyz>
2023-11-27 14:30:47 -0500

    Start article archive

---
+++ articles.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Flooded Cove - Fun</title>
+		<link rel="stylesheet" href="style.css">
+		<link rel="icon" type="image/x-icon" href="/assets/images/favicon.png">
+
+	</head>
+	<body>
+		<div id="container">
+			<div class="topbar"></div>
+			<div id="flex">
+				<main>
+					<div class="title">Important ttrpg articles</div>
+					<div class="content">
+						<p>
+These are articles that should be preserved and easily accessible for posterity sake. There is no theme other than they are all table-top related. These list is woefully incomplete. IF you have an article that you feel should be collected here, feel free to reach out via email at shocktohp@floodedrealms.com or over Twitter.
+						</p>
+						<ul>
+							<li><a href="https://clericswearringmail.blogspot.com/2020/06/how-to-make-tsr-nostalgic-map-using-gimp.html">Make TSR style maps wiht GIMP - Clerics Wear Ringmail</a></li>
+							       <li><a href="http://nagorascorner.blogspot.com/2019/12/rebuilding-elves_23.html?m=1">Rebuilding Elves - Nagora's Corner</a></li>
+							       <li><a href="http://lordgwydion.blogspot.com/2023/04/choosing-your-ruleset-as-difficulty.html">Rules as Difficulty - Lord Gwydion</a></li>
+							       <li><a href="https://princeofnothingblogs.wordpress.com/2022/05/02/that-is-why-you-fail-an-analysis-of-the-corrosive-effect-of-artpunkmanry-in-the-essay-never-as-written-by-luka-rejec/">Why you fail (response to 'Never as Written' by Luka Rejec - Prince of Nothing</a></li>
+							       <li><a href="https://thealexandrian.net/wordpress/45711/roleplaying-games/xandering-the-dungeon-addendum-how-to-use-a-melan-diagram">Using a Melan Diagram - The Alexandrian</a></li>
+					</div>
+				</main>
+				<aside>
+					<?php include 'nav.html'; ?>
+				</aside>
+			</div>
+		</div>
+
+	</body>
+</html>
